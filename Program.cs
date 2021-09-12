b3575604f5c38bcd415c308ad0d5b0707b59e433
Lilian Schall <lilian.schall@epita.fr>
2021-09-12 13:37:32 +0200

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MemeJudger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
